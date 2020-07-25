f11bd9ff9b6cd75b58894875af82229bff5f51fb
Theo Gilbert <theo.gilbert@etu.univ-tours.fr>
2020-07-25 19:48:20 +0200

    refactor(cpu_probe): move calculation part out of CpuProbe class

--- src/probe/cpu.rs
+++ src/probe/cpu.rs
@@ -1,113 +1,106 @@
-use crate::probe::{ProcessMetric, Error, Metric, Probe, procfs};
-use crate::values::PercentValue;
-use crate::process::PID;
-use crate::probe::procfs::Stat;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
-struct ProcessCpuData {
-    reader: procfs::ProcfsReader<procfs::PidStat>,
-    prev_stat: procfs::PidStat,
-}
+use crate::probe::{Error, Metric, Probe, ProcessMetric, procfs};
+use crate::probe::procfs::{PidStat, Stat};
+use crate::process::PID;
+use crate::values::PercentValue;
 
-struct StatData {
-    reader: procfs::ProcfsReader<procfs::Stat>,
-    prev_stat: procfs::Stat,
-    run_time_diff: u64,
-}
-
-pub(crate) struct CpuProbe {
-    processes_data: HashMap<PID, ProcessCpuData>,
-    stat_data: StatData,
+pub struct CpuProbe {
+    processes_readers: HashMap<PID, procfs::ProcfsReader<procfs::PidStat>>,
+    stat_reader: procfs::ProcfsReader<procfs::Stat>,
+    calculator: CpuUsageCalculator,
 }
 
 
-// Idea for testing: move computing part out of CpuProbe. CpuProble only handles file parts,
-// and defers probe/init_iteration calls to a sub computing class. We would only need to test that
-// instance
 impl CpuProbe {
     pub fn new() -> Result<Self, Error> {
-        let mut stat_file = procfs::ProcfsReader::new("stat")
+        let mut stat_reader = procfs::ProcfsReader::new("stat")
             .or_else(|e| Err(Error::IOError(e.to_string())))?;
 
-        let stat_data = stat_file
-            .read()
+        let stat_data = stat_reader.read()
             .or_else(|e| Err(Error::ProbingError(e.to_string())))?;
 
         Ok(CpuProbe {
-            processes_data: HashMap::new(),
-            stat_data: StatData {
-                reader: stat_file,
-                prev_stat: stat_data,
-                run_time_diff: 0,
-            },
+            processes_readers: HashMap::new(),
+            stat_reader,
+            calculator: CpuUsageCalculator::new(stat_data),
         })
     }
 
-    fn create_process_data(pid: PID) -> Result<ProcessCpuData, Error> {
-        let mut stat_file = procfs::ProcfsReader::new_for_pid(pid, "stat")
-            .or_else(|e| Err(Error::IOError(e.to_string())))?;
-
-        let pid_stat = stat_file
-            .read()
-            .or_else(|e| Err(Error::ProbingError(e.to_string())))?;
-
-        Ok(ProcessCpuData { reader: stat_file, prev_stat: pid_stat })
+    fn init_process_reader(pid: PID) -> Result<procfs::ProcfsReader<procfs::PidStat>, Error> {
+        procfs::ProcfsReader::new_for_pid(pid, "stat")
+            .or_else(|e| Err(Error::IOError(e.to_string())))
     }
 
-    /// Returns a mut reference to `ProcessCpuData` associated with `pid`. If it does not exists,
-    /// this function will create it first.
-    ///
-    /// # Arguments
-    ///  * `pid`: The ID of the process forr which to retrieve the `ProcessCpuData`
-    ///
-    fn get_process_data(&mut self, pid: PID) -> Result<&mut ProcessCpuData, Error> {
-        Ok(match self.processes_data.entry(pid) {
+    fn get_process_reader(&mut self, pid: PID) -> Result<&mut procfs::ProcfsReader<procfs::PidStat>, Error> {
+        Ok(match self.processes_readers.entry(pid) {
             Entry::Occupied(o) => o.into_mut(),
-            Entry::Vacant(v) => v.insert(Self::create_process_data(pid)?)
+            Entry::Vacant(v) => v.insert(Self::init_process_reader(pid)?)
         })
     }
 }
 
 impl Probe for CpuProbe {
     fn init_iteration(&mut self) -> Result<(), Error> {
-        let new_stat: Stat = self.stat_data.reader
+        let new_stat: Stat = self.stat_reader
             .read()
             .or_else(|e| Err(Error::ProbingError(e.to_string())))?;
 
-        self.stat_data.run_time_diff = new_stat.running_time() - self.stat_data.prev_stat.running_time();
-        self.stat_data.prev_stat = new_stat;
+        self.calculator.update_stat_data(new_stat);
 
         Ok(())
     }
 
     fn probe(&mut self, pid: PID) -> Result<ProcessMetric, Error> {
-        let mut proc_data = self.get_process_data(pid)?;
+        let mut proc_reader = self.get_process_reader(pid)?;
 
-        let new_pid_stat = proc_data.reader
+        let new_pid_stat = proc_reader
             .read()
             .or_else(|e| Err(Error::ProbingError(e.to_string())))?;
 
-        let pid_runtime_diff = new_pid_stat.running_time() - proc_data.prev_stat.running_time();
-        proc_data.prev_stat = new_pid_stat;
-
-        let ratio = pid_runtime_diff as f64 / self.stat_data.run_time_diff as f64;
-        let percent = (100. * ratio) as f32;
-
-        let value = PercentValue::new(percent)
-            .or_else(|_e| {
-                Err(Error::ProbingError(format!("Invalid percent: {}", percent)))
-            })?;
-
-        Ok(ProcessMetric { pid, value: Metric::CpuUsage(value) })
+        self.calculator.calculate_pid_usage(pid, new_pid_stat)
     }
 }
 
 struct CpuUsageCalculator {
+    processes_prev_stats: HashMap<PID, procfs::PidStat>,
+    prev_global_stat: procfs::Stat,
+    global_runtime_diff: f64,
 }
 
 impl CpuUsageCalculator {
-    fn update_stat_data(&mut self, stat_data: StatData) -> () {
+    pub fn new(init_stat_data: procfs::Stat) -> Self {
+        CpuUsageCalculator {
+            processes_prev_stats: HashMap::new(),
+            prev_global_stat: init_stat_data,
+            global_runtime_diff: 0.,
+        }
+    }
 
+    pub fn update_stat_data(&mut self, stat_data: Stat) -> () {
+        self.global_runtime_diff = (stat_data.running_time() - self.prev_global_stat.running_time()) as f64;
+        self.prev_global_stat = stat_data;
+    }
+
+    pub fn calculate_pid_usage(&mut self, pid: PID, pid_stat_data: PidStat) -> Result<ProcessMetric, Error> {
+        let last_iter_runtime = match self.processes_prev_stats.get(&pid) {
+            Some(stat_data) => stat_data.running_time(),
+            None => 0
+        };
+
+        let pid_runtime_diff = pid_stat_data.running_time() - last_iter_runtime;
+        self.processes_prev_stats.insert(pid, pid_stat_data);
+
+        let ratio = pid_runtime_diff as f64 / self.global_runtime_diff;
+        let percent = (100. * ratio) as f32;
+
+        let value = PercentValue::new(percent)
+            .or_else(|_e| {
+                Err(Error::ProbingError(format!("Invalid CPU usage value for PID {} : {}",
+                                                pid, percent)))
+            })?;
+
+        Ok(ProcessMetric { pid, value: Metric::CpuUsage(value) })
     }
 }
