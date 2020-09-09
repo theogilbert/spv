1cc457c55f42a79477ed22ed9b1916b6dbe25400
Theo Gilbert <theo.gilbert@etu.univ-tours.fr>
2020-09-09 23:06:16 +0200

    feat(main): update main to launch un-parameterized app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+use std::io;
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+use spv::app::{SpvApplication, SpvContext};
+use spv::triggers::TriggersEmitter;
 
 fn main() {
-    println!("Hello world!");
+    let (tx, rx) = channel();
+
+    TriggersEmitter::launch_async(tx, Duration::from_secs(1));
+    SpvApplication::new(rx, SpvContext::new())
+        .run();
 }
