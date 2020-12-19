1fa0cfaf3d859d03f2929e6eceeb5bf8dd72031f
Theo Gilbert <theovgilbert@gmail.com>
2020-12-19 15:27:33 +0100

    feat(processes): add title row in process list

--- src/ui/processes.rs
+++ src/ui/processes.rs
@@ -1,16 +1,17 @@
 use tui::Frame;
-use tui::layout::{Constraint, Direction, Layout, Rect};
+use tui::layout::{Alignment, Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, List, ListItem, ListState};
+use tui::text::Span;
+use tui::widgets::{Block, Borders, List, ListItem, ListState, Paragraph};
 
 use crate::core::metrics::Archive;
 use crate::core::process_view::{PID, ProcessMetadata};
 use crate::ui::terminal::TuiBackend;
 
-/// Maximum amount of characters to display for a command (including ".." if the cmd is too long)
-const MAX_COMMAND_LENGTH: usize = 16;
-/// Maximum amount of characters to display for a metric
-const MAX_METRICS_LENGTH: usize = 10;
+/// Width of the process name column
+const CMD_COL_WIDTH: usize = 16;
+/// Width of the metrics values column
+const METRICS_COL_WIDTH: usize = 10;
 
 pub struct ProcessList {
     processes: Vec<ProcessMetadata>,
@@ -28,108 +29,70 @@ impl Default for ProcessList {
     }
 }
 
-// TODO add first row Process name - [%/bps]
+
 impl ProcessList {
-    pub fn render(&mut self, frame: &mut Frame<TuiBackend>, chunk: Rect, metrics: &Archive,
+    /// Renders the processes assigned through the
+    /// [`set_processes`](struct.ProcessList#method.set_processes) method
+    ///
+    /// # Arguments
+    ///   * `frame`: The `Frame` on which to render the process list widget
+    ///   * `chunk`: The region within the `frame` reserved for this widget
+    ///   * `archive`: The metrics archive, to display the current metric of each process
+    ///   * `label`: The name of the metric to display
+    pub fn render(&mut self, frame: &mut Frame<TuiBackend>, chunk: Rect, archive: &Archive,
                   label: &str) {
-        let columns_chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .constraints(
-                [
-                    Constraint::Min(MAX_COMMAND_LENGTH as u16 + 2),  // processes names
-                    Constraint::Length(MAX_METRICS_LENGTH as u16),  // processes metrics
-                ].as_ref()
-            )
+        let rows_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints([Constraint::Length(2), Constraint::Min(1)])
             .split(chunk);
 
-        self.render_name_column(frame, columns_chunks[0]);
-        self.render_metric_column(frame, columns_chunks[1], metrics, label);
+        let (proc_chunk, metric_chunk) = Self::split_column_chunks(rows_chunks[1]);
+
+        Self::render_title_row(frame, rows_chunks[0], archive, label);
+        self.render_name_column(frame, proc_chunk);
+        self.render_metric_column(frame, metric_chunk, archive, label);
     }
 
-    fn shortened_command_name(process_metadata: &ProcessMetadata) -> String {
-        if process_metadata.command().len() > MAX_COMMAND_LENGTH {
-            format!("{}..", &process_metadata.command()[0..MAX_COMMAND_LENGTH - 2])
-        } else {
-            process_metadata.command().to_string()
-        }
-    }
-
-    fn render_name_column(&mut self, frame: &mut Frame<TuiBackend>, chunk: Rect) {
-        let processes_names: Vec<_> = self.processes.iter()
-            .map(|pm| Self::shortened_command_name(pm))
-            .collect();
-
-        let items: Vec<ListItem> = processes_names.iter()
-            .map(|cmd| ListItem::new(cmd.as_str()))
-            .collect();
-
-        let list = Self::build_default_list_widget(items)
-            .block(Block::default().borders(Borders::LEFT | Borders::TOP | Borders::BOTTOM))
-            .highlight_symbol(">> ");
-
-        frame.render_stateful_widget(list, chunk, &mut self.state);
-    }
-
-    fn render_metric_column(&mut self, frame: &mut Frame<TuiBackend>, chunk: Rect, metrics: &Archive, label: &str) {
-        let str_metrics: Vec<String> = self.processes.iter()
-            .map(|pm| self.formatted_process_metric(pm, metrics, label))
-            .collect();
-
-        let items: Vec<ListItem> = str_metrics.iter()
-            .map(|pm| ListItem::new(pm.as_str()))
-            .collect();
-
-        let list = Self::build_default_list_widget(items)
-            .block(Block::default().borders(Borders::TOP | Borders::BOTTOM));
-
-        frame.render_stateful_widget(list, chunk, &mut self.state);
-    }
-
-    fn formatted_process_metric(&self, process: &ProcessMetadata, metrics: &Archive,
-                                label: &str) -> String {
-        let m = metrics.last(label, process.pid())
-            .expect("Error getting current metric");
-
-        self.align_metric_right(m.concise_repr())
-    }
-
-    fn align_metric_right(&self, text: String) -> String {
-        let indent = MAX_METRICS_LENGTH.checked_sub(text.len() + 1)
-            .unwrap_or(1)
-            .max(1);
-        format!("{:indent$}{} ", "", text, indent = indent)
-    }
-
-    fn build_default_list_widget(items: Vec<ListItem>) -> List {
-        List::new(items)
-            .style(Style::default().fg(Color::White))
-            .highlight_style(Style::default().add_modifier(Modifier::BOLD))
-    }
-
+    /// Define the processes to render in the process list
+    /// The processes will be displayed in the list in the same order as they appear in `processes`
     pub fn set_processes(&mut self, processes: Vec<ProcessMetadata>) {
         let index_opt = if self.processes.is_empty() {
             None
         } else {
-            Some(Self::retrieve_previously_selected_index(&processes, self.selected_pid))
+            Some(Self::retrieve_index_of_previously_selected_pid(&processes, self.selected_pid))
         };
 
         self.processes = processes;
         self.select(index_opt);
     }
 
-    fn retrieve_previously_selected_index(processes: &[ProcessMetadata],
-                                          selected_pid: Option<PID>) -> usize {
-        let mut selected_idx = 0;
+    /// Focus the previous process
+    pub fn previous(&mut self) {
+        let prev_idx = self.state.selected()
+            .map(|s| if s > 0 { s - 1 } else { 0 })
+            .unwrap_or(0);
 
-        if let Some(selected_pid) = selected_pid {
-            selected_idx = processes.iter()
-                .position(|pm| pm.pid() == selected_pid)
-                .unwrap_or(0); // If PID does not exist anymore, select first process
-        }
-
-        selected_idx
+        self.select(Some(prev_idx));
     }
 
+    /// Focus the next process
+    pub fn next(&mut self) {
+        let next_idx = self.state.selected()
+            .map(|s| s + 1)
+            .unwrap_or(0);
+
+        self.select(Some(next_idx));
+    }
+
+    /// Returns the selected `&ProcessMetadata`
+    pub fn selected(&self) -> Option<&ProcessMetadata> {
+        match self.selected_pid {
+            None => None,
+            Some(pid) => self.processes.iter().find(|pm| pm.pid() == pid)
+        }
+    }
+
+    /// Select the process at the given index
     fn select(&mut self, index: Option<usize>) {
         match index {
             None => {
@@ -145,27 +108,110 @@ impl ProcessList {
         }
     }
 
-    pub fn previous(&mut self) {
-        let prev_idx = self.state.selected()
-            .map(|s| if s > 0 { s - 1 } else { 0 })
-            .unwrap_or(0);
 
-        self.select(Some(prev_idx));
-    }
-
-    pub fn next(&mut self) {
-        let next_idx = self.state.selected()
-            .map(|s| s + 1)
-            .unwrap_or(0);
-
-        self.select(Some(next_idx));
-    }
-
-    pub fn selected(&self) -> Option<&ProcessMetadata> {
-        match self.selected_pid {
-            None => None,
-            Some(pid) => self.processes.iter().find(|pm| pm.pid() == pid)
+    /// Processes are displayed in a list, sorted by their metric values
+    /// From one frame to the other, the same process may have a different position in the list
+    /// This function returns the new position of the selected process in the given `processes` list
+    fn retrieve_index_of_previously_selected_pid(processes: &[ProcessMetadata],
+                                                 selected_pid: Option<PID>) -> usize {
+        match selected_pid {
+            Some(selected_pid) => {
+                processes.iter()
+                    .position(|pm| pm.pid() == selected_pid)
+                    .unwrap_or(0) // If PID does not exist anymore, select first process
+            }
+            None => 0
         }
+    }
+
+    /// Splits a `Rect` into two:
+    ///   - One that will contain the command name
+    ///   - One that will contain the metric value
+    fn split_column_chunks(chunk: Rect) -> (Rect, Rect) {
+        let columns_chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(
+                [
+                    Constraint::Min(CMD_COL_WIDTH as u16 + 2),  // processes names
+                    Constraint::Min(METRICS_COL_WIDTH as u16),  // processes metrics
+                ].as_ref()
+            )
+            .split(chunk);
+
+        (columns_chunks[0], columns_chunks[1])
+    }
+
+    fn render_title_row(frame: &mut Frame<TuiBackend>, chunk: Rect, archive: &Archive, label: &str) {
+        let (proc_chunk, metric_chunk) = Self::split_column_chunks(chunk);
+
+        let proc_paragraph = Paragraph::new("Process name")
+            .block(Block::default().borders(Borders::LEFT | Borders::TOP))
+            .alignment(Alignment::Center);
+
+        let metric_text = format!("{} ", archive.label_unit(label).unwrap());
+        let metric_title = Paragraph::new(metric_text)
+            .block(Block::default().borders(Borders::TOP))
+            .alignment(Alignment::Right);
+
+        frame.render_widget(proc_paragraph, proc_chunk);
+        frame.render_widget(metric_title, metric_chunk);
+    }
+
+    fn render_name_column(&mut self, frame: &mut Frame<TuiBackend>, chunk: Rect) {
+        let processes_names: Vec<_> = self.processes.iter()
+            .map(|pm| Self::shortened_command_name(pm))
+            .collect();
+
+        let items: Vec<ListItem> = processes_names.iter()
+            .map(|cmd| ListItem::new(cmd.as_str()))
+            .collect();
+
+        let list = Self::build_default_list_widget(items)
+            .block(Block::default().borders(Borders::LEFT | Borders::BOTTOM))
+            .highlight_symbol(">> ");
+
+        frame.render_stateful_widget(list, chunk, &mut self.state);
+    }
+
+
+    /// Returns the formatted command name of `process_metadata` so that its length does not exceed
+    /// `MAX_COMMAND_LENGTH` characters
+    fn shortened_command_name(process_metadata: &ProcessMetadata) -> String {
+        if process_metadata.command().len() > CMD_COL_WIDTH {
+            format!("{}..", &process_metadata.command()[0..CMD_COL_WIDTH - 2])
+        } else {
+            process_metadata.command().to_string()
+        }
+    }
+
+    fn render_metric_column(&mut self, frame: &mut Frame<TuiBackend>, chunk: Rect, archive: &Archive, label: &str) {
+        let str_metrics: Vec<String> = self.processes.iter()
+            .map(|pm| self.formatted_process_metric(pm, archive, label))
+            .collect();
+
+        let items: Vec<ListItem> = str_metrics.iter()
+            .map(|pm| ListItem::new(pm.as_str()))
+            .collect();
+
+        let list = Self::build_default_list_widget(items)
+            .block(Block::default().borders(Borders::BOTTOM));
+
+        frame.render_stateful_widget(list, chunk, &mut self.state);
+    }
+
+    fn formatted_process_metric(&self, process: &ProcessMetadata, metrics: &Archive,
+                                label: &str) -> String {
+        let m = metrics.last(label, process.pid())
+            .expect("Error getting current metric");
+
+        format!("{:>width$} ", m.concise_repr(), width = METRICS_COL_WIDTH - 1)  // - 1 because of the trailing space
+    }
+
+
+    fn build_default_list_widget(items: Vec<ListItem>) -> List {
+        List::new(items)
+            .style(Style::default().fg(Color::White))
+            .highlight_style(Style::default().add_modifier(Modifier::BOLD))
     }
 }
 
@@ -174,7 +220,7 @@ impl ProcessList {
 mod test_align_right {
     use rstest::*;
 
-    use crate::ui::processes::{ProcessList, MAX_METRICS_LENGTH};
+    use crate::ui::processes::{METRICS_COL_WIDTH, ProcessList};
 
     #[fixture]
     fn process_list() -> ProcessList {
@@ -195,7 +241,7 @@ mod test_align_right {
         let aligned = process_list.align_metric_right(input.to_string());
 
         assert!(aligned.ends_with(&format!("{} ", input)));
-        assert_eq!(aligned.len(), MAX_METRICS_LENGTH)
+        assert_eq!(aligned.len(), METRICS_COL_WIDTH)
     }
 
     #[rstest]
