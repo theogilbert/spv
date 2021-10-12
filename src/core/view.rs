9e9336ac3f3a6a8726a198a0fb85c24c6f981623
Theo Gilbert <theovgilbert@gmail.com>
2021-10-12 22:11:25 +0200

    test(view): test & doc MetricView and Overview

--- src/core/view.rs
+++ src/core/view.rs
@@ -1,9 +1,15 @@
+//! View of collected metrics
+
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::time::Duration;
 
 use crate::core::metrics::Metric;
 use crate::core::process_view::Pid;
 
+/// Snapshot of all collected metrics of a single process, from a single probe
+/// 
+/// Refer to the [`MetricCollector`](crate::core::collection::MetricCollector) trait to instanciate a `MetricView`
 pub struct MetricView<'a> {
     metrics: Vec<&'a dyn Metric>,
     resolution: Duration,
@@ -11,40 +17,72 @@ pub struct MetricView<'a> {
 }
 
 impl<'a> MetricView<'a> {
-    pub fn new(metrics: Vec<&'a dyn Metric>, resolution: Duration, default: &'a dyn Metric) -> Self {
+    pub(crate) fn new(metrics: Vec<&'a dyn Metric>, resolution: Duration, default: &'a dyn Metric) -> Self {
         Self { metrics, resolution, default }
     }
 
+    /// Returns a slice of the metrics contained in this view.
+    /// The slice only covers the last metrics covered by the `span` parameter.
+    ///
+    /// For example, if the view has a resolution of 1 second, `extract(Duration::from_secs(10))`
+    /// will return the last 10 metrics.
+    ///
+    /// # Arguments
+    ///  * span: Indicates from how long ago the metrics should be compared
     pub fn extract(&'a self, span: Duration) -> &[&'a dyn Metric] {
         let collected_metrics_count = self.metrics.len();
-        let expected_metrics_count = self.expected_metrics(span);
+        let expected_metrics_count = self.calculate_number_of_expected_metrics(span);
         let skipped_metrics = collected_metrics_count.saturating_sub(expected_metrics_count);
 
         &self.metrics[skipped_metrics..]
     }
 
-    /// Indicates how many metrics should be returned by history() with the given span, according
-    /// to this archive's resolution.
-    /// Note that The value returned by this function is not a guarantee. History() may return less.
+    /// Indicates how many metrics should be returned by extract() with the given span, according
+    /// to this view's resolution.
+    /// Note that the value returned by this function is only an upper bound.
+    /// `self.extract()` may return less metrics.
     ///
     /// # Arguments
     ///  * span: Indicates from how long ago should metrics be returned
-    fn expected_metrics(&self, span: Duration) -> usize {
+    fn calculate_number_of_expected_metrics(&self, span: Duration) -> usize {
         (span.as_secs() / self.resolution.as_secs()) as usize
     }
 
+    /// Returns the unit representation of the metrics contained in this view
     pub fn unit(&self) -> &'static str {
         self.default.unit()
     }
 
+    /// Indicates the `Duration` step between each metric
     pub fn resolution(&self) -> Duration { self.resolution }
 
+    /// Returns the latest collected metric, or its default value if no metric has
+    /// been collected for this process.
     pub fn last_or_default(&self) -> &dyn Metric {
         *(self.metrics.last()
             .unwrap_or(&self.default))
     }
+
+    /// Returns the greatest f64 value of the metric in the given span. See [`MetricView::new()`](#method.extract) for
+    /// the behavior of `span`.
+    ///
+    /// If the metrics have a cardinality greater than one, the max f64 component of the metric is
+    /// used for the comparison.
+    ///
+    /// # Arguments
+    ///  * span: Indicates from how long ago the metrics should be compared
+    pub fn max_f64(&self, span: Duration) -> f64 {
+        self.extract(span)
+            .iter()
+            .map(|m| m.max_value())
+            .max_by(|f1, f2| f1.partial_cmp(f2).unwrap_or(Ordering::Equal))
+            .unwrap_or(0.)
+    }
 }
 
+/// Overview of a single probe's latest metrics, for all running processes
+///
+/// Refer to the [`MetricCollector`](crate::core::collection::MetricCollector) trait to instanciate a `MetricsOverview`
 pub struct MetricsOverview<'a> {
     last_metrics: HashMap<Pid, &'a dyn Metric>,
     default: &'a dyn Metric,
@@ -55,11 +93,17 @@ impl<'a> MetricsOverview<'a> {
         Self { last_metrics, default }
     }
 
+    /// Returns the latest collected `Metric` for a given process
+    /// If no metric has been collected for this process, the default `Metric` value is returned.
+    ///
+    /// # Arguments
+    ///  * pid: The ID of the process
     pub fn last_or_default(&self, pid: Pid) -> &dyn Metric {
         *(self.last_metrics.get(&pid)
             .unwrap_or(&self.default))
     }
 
+    /// Returns the unit representation of the metrics contained in this view
     pub fn unit(&self) -> &'static str {
         self.default.unit()
     }
@@ -75,14 +119,14 @@ mod test_metric_view {
     use crate::core::view::MetricView;
     use crate::core::view::test_helpers::produce_metrics_collection;
 
-    fn build_view(collection: &MetricCollection<PercentMetric>) -> MetricView {
+    fn build_view_of_pid_0(collection: &MetricCollection<PercentMetric>) -> MetricView {
         collection.view(0, Duration::from_secs(1))
     }
 
     #[test]
     fn test_last_or_default_should_be_latest_metric_when_exists() {
-        let collection = produce_metrics_collection(1, 2);
-        let view = build_view(&collection);
+        let collection = produce_metrics_collection(1, vec![0., 1.]);
+        let view = build_view_of_pid_0(&collection);
 
         assert_eq!(view.last_or_default(), &PercentMetric::new(1.));
     }
@@ -90,7 +134,7 @@ mod test_metric_view {
     #[test]
     fn test_last_or_default_should_be_default_when_pid_unknown() {
         let collection = MetricCollection::<PercentMetric>::new();
-        let view = build_view(&collection);
+        let view = build_view_of_pid_0(&collection);
 
         assert_eq!(view.last_or_default(), &PercentMetric::default());
     }
@@ -98,7 +142,7 @@ mod test_metric_view {
     #[test]
     fn test_unit_should_be_metric_unit() {
         let collection = MetricCollection::<PercentMetric>::new();
-        let view = build_view(&collection);
+        let view = build_view_of_pid_0(&collection);
 
         assert_eq!(view.unit(), PercentMetric::default().unit());
     }
@@ -114,29 +158,29 @@ mod test_metric_view {
     #[test]
     fn test_extract_should_extract_nothing_if_collection_is_empty() {
         let collection = MetricCollection::<PercentMetric>::new();
-        let view = build_view(&collection);
+        let view = build_view_of_pid_0(&collection);
 
         assert_eq!(view.extract(Duration::from_secs(60)), &[]);
     }
 
     #[test]
     fn test_extract_should_return_only_last_metric_if_span_equals_duration() {
-        let collection = produce_metrics_collection(1, 10);
-        let view = build_view(&collection);
+        let collection = produce_metrics_collection(1, vec![0., 1.]);
+        let view = build_view_of_pid_0(&collection);
 
         assert_eq!(view.extract(view.resolution()), &[view.last_or_default()]);
     }
 
     #[test]
     fn test_extract_should_return_two_last_metric_if_span_is_double_duration() {
-        let collection = produce_metrics_collection(1, 10);
-        let view = build_view(&collection);
+        let collection = produce_metrics_collection(1, vec![0., 1., 2., 3.]);
+        let view = build_view_of_pid_0(&collection);
 
         let extract = view.extract(view.resolution() * 2);
 
         let expected: &[&dyn Metric; 2] = &[
-            &PercentMetric::new(8.),
-            &PercentMetric::new(9.)
+            &PercentMetric::new(2.),
+            &PercentMetric::new(3.)
         ];
 
         assert_eq!(extract, expected);
@@ -144,14 +188,38 @@ mod test_metric_view {
 
     #[test]
     fn test_should_only_return_existing_items_when_span_greater_than_metric_count() {
-        let collection = produce_metrics_collection(1, 10);
-        let view = build_view(&collection);
+        let collection = produce_metrics_collection(1, vec![0., 1., 2.]);
+        let view = build_view_of_pid_0(&collection);
 
         let extract = view.extract(view.resolution() * 20);
 
-        assert_eq!(extract.len(), 10);
+        assert_eq!(extract.len(), 3);
         assert_eq!(extract[0], &PercentMetric::new(0.));
-        assert_eq!(extract[9], &PercentMetric::new(9.));
+        assert_eq!(extract[2], &PercentMetric::new(2.));
+    }
+
+    #[test]
+    fn test_max_f64_should_return_max_value() {
+        let collection = produce_metrics_collection(1, vec![10., 0., 2.]);
+        let view = build_view_of_pid_0(&collection);
+
+        assert_eq!(view.max_f64(Duration::from_secs(3)), 10.);
+    }
+
+    #[test]
+    fn test_max_f64_should_not_return_values_out_of_span() {
+        let collection = produce_metrics_collection(1, vec![10., 0., 2.]);
+        let view = build_view_of_pid_0(&collection);
+
+        assert_eq!(view.max_f64(Duration::from_secs(2)), 2.);
+    }
+
+    #[test]
+    fn test_max_f64_should_return_0_when_empty() {
+        let collection = MetricCollection::<PercentMetric>::new();
+        let view = build_view_of_pid_0(&collection);
+
+        assert_eq!(view.max_f64(Duration::from_secs(2)), 0.);
     }
 }
 
@@ -167,22 +235,25 @@ mod test_metric_overview {
         collection.overview()
     }
 
+    #[test]
     fn test_unit_should_be_default_metric_unit() {
-        let collection = produce_metrics_collection(2, 2);
+        let collection = produce_metrics_collection(2, vec![0., 1.]);
         let overview = build_overview(&collection);
 
         assert_eq!(overview.unit(), PercentMetric::default().unit());
     }
 
+    #[test]
     fn test_last_or_default_should_return_last_when_proc_has_metrics() {
-        let collection = produce_metrics_collection(2, 2);
+        let collection = produce_metrics_collection(2, vec![0., 1.]);
         let overview = build_overview(&collection);
 
         assert_eq!(overview.last_or_default(0), &PercentMetric::new(1.));
     }
 
+    #[test]
     fn test_last_or_default_should_return_default_when_pid_is_unknown() {
-        let collection = produce_metrics_collection(2, 2);
+        let collection = produce_metrics_collection(2, vec![0., 1.]);
         let overview = build_overview(&collection);
 
         assert_eq!(overview.last_or_default(2), &PercentMetric::default());
@@ -198,12 +269,12 @@ mod test_helpers {
     /// Return collection of PercentMetric containing metrics for `proc_count` processes.<br/>
     /// The Pid values range from `0` to `proc_count - 1`<br/>
     /// To each processes are associated the same PercentMetric values, ranging from `0` to `metrics_count`
-    pub(crate) fn produce_metrics_collection(proc_count: usize, metrics_count: usize) -> MetricCollection<PercentMetric> {
+    pub(crate) fn produce_metrics_collection(proc_count: usize, values: Vec::<f64>) -> MetricCollection<PercentMetric> {
         let mut collection = MetricCollection::new();
 
-        for proc_idx in 0..proc_count {
-            for metric_idx in 0..metrics_count {
-                collection.push(proc_idx as Pid, PercentMetric::new(metric_idx as f64));
+        for value in values.into_iter() {
+            for proc_idx in 0..proc_count {
+                collection.push(proc_idx as Pid, PercentMetric::new(value));
             }
         }
 
