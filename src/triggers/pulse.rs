f33824adccb64d8dfb80093b08fa0fbabccdb941
Theo Gilbert <theovgilbert@gmail.com>
2021-10-14 23:03:35 +0200

    test(pulse): test accuracy of Pulse

--- src/triggers/pulse.rs
+++ src/triggers/pulse.rs
@@ -1,5 +1,5 @@
-use std::time::{Duration, Instant};
 use std::thread;
+use std::time::{Duration, Instant};
 
 pub struct Pulse {
     last_tick: Instant,
@@ -19,5 +19,31 @@ impl Pulse {
         thread::sleep(self.refresh_period - elapsed);
 
         self.last_tick = Instant::now();
+    }
+}
+
+#[cfg(test)]
+mod test_pulse {
+    use std::time::{Duration, SystemTime};
+
+    use crate::triggers::pulse::Pulse;
+
+    #[test]
+    fn test_should_respect_refresh_period() {
+        let tolerance_in_ms = 2;
+        let mut pulse = Pulse::new(Duration::from_millis(10));
+
+        let start = SystemTime::now();
+
+        for _ in 0..10 {
+            pulse.pulse();
+        }
+
+        let elapsed = SystemTime::now().duration_since(start)
+            .expect("Error calculating pulse test elapsed time");
+
+        // 10 pulses at a refresh period of 10ms should take ~100ms to complete
+        assert!(elapsed.as_millis() > 100 - tolerance_in_ms);
+        assert!(elapsed.as_millis() < 100 + tolerance_in_ms);
     }
 }
