98b669b544a3edcf4ba716dd9bae5d0c3d42cebf
theogilbert <theovgilbert@gmail.com>
2021-12-13 18:19:44 +0100

    feat(process): calculate process spawn time when fetching metadata

--- src/procfs/process.rs
+++ src/procfs/process.rs
@@ -1,26 +1,32 @@
 //! Process discovery
 
-use std::fs::{read_dir, DirEntry, File};
+use std::fs::{read_dir, DirEntry};
 use std::io;
-use std::io::Read;
 use std::path::PathBuf;
+use std::time::Duration;
 
 use thiserror::Error;
 
 use crate::core::process::{Pid, ProcessMetadata, ProcessScanner};
+use crate::core::time::Timestamp;
 use crate::core::Error as CoreError;
+use crate::procfs::parsers::{
+    Comm, PidStat, ProcessDataReader, ReadProcessData, ReadSystemData, SystemDataReader, Uptime,
+};
+use crate::procfs::sysconf::clock_ticks;
+use crate::procfs::ProcfsError;
 
 /// Errors internal to the process module
 #[derive(Error, Debug)]
 enum Error {
     #[error("Directory PID has invalid syntax: '{0:?}'")]
     NotProcessDir(String),
+    #[error("Failed to parse system data '{0:?}': '{1:?}'")]
+    SystemParsingFailure(String, #[source] ProcfsError),
     #[error("Failed to read the content of directory '{0:?}'")]
     ProcessScanningFailure(PathBuf, #[source] io::Error),
-    #[error("Error while parsing process directory '{0:?}'")]
-    ProcessParsing(PathBuf, #[source] io::Error),
-    #[error("PID is invalid: '{0:?}'")]
-    InvalidPID(Pid),
+    #[error("Error while parsing process data '{1:?}' for process {0:?}: '{2:?}'")]
+    ProcessParsing(Pid, String, #[source] anyhow::Error),
 }
 
 impl From<Error> for CoreError {
@@ -30,18 +36,29 @@ impl From<Error> for CoreError {
 }
 
 /// Implementation of ProcessScanner that uses the `/proc` Linux virtual directory as source
-#[derive(Default)]
 pub struct ProcfsScanner {
     proc_dir: PathBuf,
+    comm_reader: Box<dyn ReadProcessData<Comm>>,
+    stat_reader: Box<dyn ReadProcessData<PidStat>>,
+    boot_time: Timestamp,
 }
 
 /// Scan running processes on a Linux host by scanning the content of /proc directory
 impl ProcfsScanner {
     /// Returns a new ProcfsScanner instance
-    pub fn new() -> ProcfsScanner {
-        ProcfsScanner {
+    pub fn new() -> Result<ProcfsScanner, CoreError> {
+        let boot_time = SystemDataReader::<Uptime>::new()
+            .map_err(|e| Error::SystemParsingFailure("uptime".into(), e))?
+            .read()
+            .map_err(|e| Error::SystemParsingFailure("uptime".into(), e))?
+            .boot_time();
+
+        Ok(ProcfsScanner {
             proc_dir: PathBuf::from("/proc"),
-        }
+            comm_reader: Box::new(ProcessDataReader::new()),
+            stat_reader: Box::new(ProcessDataReader::new()),
+            boot_time,
+        })
     }
 
     /// Parses a PID from a directory name, if it represents an unsigned integer
@@ -49,13 +66,26 @@ impl ProcfsScanner {
     /// # Arguments
     /// * `dir_name` - An optional string slice that holds the name of a directory
     ///
-    fn extract_pid_from_proc_dir(dir_name_opt: Option<&str>) -> std::result::Result<Pid, Error> {
+    fn extract_pid_from_proc_dir(dir_name_opt: Option<&str>) -> Result<Pid, Error> {
         match dir_name_opt {
             Some(dir_name) => dir_name
                 .parse::<Pid>()
                 .map_err(|_| Error::NotProcessDir(dir_name.to_string())),
             None => Err(Error::NotProcessDir("".to_string())),
         }
+    }
+
+    /// Calculates the timestamp at which the process started
+    fn calculate_spawn_time(&mut self, pid: Pid) -> Result<Timestamp, CoreError> {
+        let clock_ticks = clock_ticks().map_err(|e| Error::SystemParsingFailure("_SC_CLK_TCK".into(), e))?;
+
+        let starttime = self
+            .stat_reader
+            .read(pid)
+            .map_err(|e| Error::ProcessParsing(pid, "stat".into(), e.into()))?
+            .starttime();
+
+        Ok(self.boot_time + Duration::from_secs(starttime / clock_ticks))
     }
 }
 
@@ -84,21 +114,15 @@ impl ProcessScanner for ProcfsScanner {
     ///
     /// # Arguments
     ///  * `pid`: The identifier of the process for which to retrieve metadata
-    fn fetch_metadata(&self, pid: Pid) -> std::result::Result<ProcessMetadata, CoreError> {
-        let mut command = String::new();
-        let comm_file_path = self.proc_dir.join(pid.to_string()).join("comm");
+    fn fetch_metadata(&mut self, pid: Pid) -> std::result::Result<ProcessMetadata, CoreError> {
+        let comm = self
+            .comm_reader
+            .read(pid)
+            .map_err(|e| Error::ProcessParsing(pid, "comm".into(), e.into()))?;
 
-        let mut file: File = File::open(comm_file_path.as_path()).map_err(|_| Error::InvalidPID(pid))?;
+        let spawntime = self.calculate_spawn_time(pid)?;
 
-        file.read_to_string(&mut command)
-            .map_err(|io_err| Error::ProcessParsing(comm_file_path, io_err))?;
-
-        if command.ends_with('\n') {
-            // Remove trailing newline
-            command.pop();
-        }
-
-        Ok(ProcessMetadata::new(pid, command))
+        Ok(ProcessMetadata::new(pid, comm.into_command(), spawntime))
     }
 }
 
@@ -133,13 +157,14 @@ mod test_pid_from_proc_dir {
 #[cfg(test)]
 mod test_pid_scanner {
     use std::fs;
-    use std::io::Write;
     use std::os::unix::fs::PermissionsExt;
     use std::path::Path;
 
+    use sn_fake_clock::FakeClock;
     use tempfile::{tempdir, NamedTempFile};
 
     use crate::core::Error as CoreError;
+    use crate::procfs::parsers::fakes::FakeProcessDataReader;
 
     use super::*;
 
@@ -161,6 +186,27 @@ mod test_pid_scanner {
 
         perms.set_mode(mode);
         fs::set_permissions(path, perms)
+    }
+
+    fn build_pid_scanner(proc_dir: PathBuf) -> ProcfsScanner {
+        ProcfsScanner {
+            proc_dir,
+            comm_reader: Box::new(FakeProcessDataReader::new()),
+            stat_reader: Box::new(FakeProcessDataReader::new()),
+            boot_time: Timestamp::now(),
+        }
+    }
+
+    fn build_metadata_fetcher(
+        comm_reader: FakeProcessDataReader<Comm>,
+        stat_reader: FakeProcessDataReader<PidStat>,
+    ) -> ProcfsScanner {
+        ProcfsScanner {
+            proc_dir: PathBuf::new(),
+            comm_reader: Box::new(comm_reader),
+            stat_reader: Box::new(stat_reader),
+            boot_time: Timestamp::now(),
+        }
     }
 
     #[test]
@@ -190,9 +236,7 @@ mod test_pid_scanner {
             );
         }
 
-        let mut proc_scanner = ProcfsScanner {
-            proc_dir: test_proc_dir.path().to_path_buf(),
-        };
+        let mut proc_scanner = build_pid_scanner(test_proc_dir.path().to_path_buf());
 
         // when we scan processes
         let mut pids = proc_scanner.scan().expect("Could not scan processes");
@@ -208,9 +252,7 @@ mod test_pid_scanner {
         let test_proc_dir = tempdir().expect("Could not create tmp dir");
         set_dir_permissions(test_proc_dir.path(), 0o000).expect("Could not set dir permissions");
 
-        let mut proc_scanner = ProcfsScanner {
-            proc_dir: test_proc_dir.path().to_path_buf(),
-        };
+        let mut proc_scanner = build_pid_scanner(test_proc_dir.path().to_path_buf());
 
         // when we scan processes
         let pids = proc_scanner.scan();
@@ -222,56 +264,53 @@ mod test_pid_scanner {
     }
 
     #[test]
-    fn test_process_metadata() {
-        let test_proc_dir = tempdir().expect("Could not create tmp dir");
+    fn test_process_metadata_has_correct_cmd() {
+        let mut comm_reader = FakeProcessDataReader::<Comm>::new();
+        let mut stat_reader = FakeProcessDataReader::<PidStat>::new();
 
-        create_tempdir("123", test_proc_dir.path()).expect("Could not create process dir");
+        comm_reader.set_pid_sequence(123, vec![Comm::new("test_cmd")]);
+        stat_reader.set_pid_sequence(123, vec![PidStat::new(0, 0, 0, 0, 0)]);
 
-        let mut comm_file =
-            create_tempfile("comm", test_proc_dir.path().join("123").as_os_str()).expect("Could not create comm file");
-
-        comm_file.write(b"test_cmd").expect("Could not write to comm file"); // The process 123's command is test_cmd
-
-        let proc_scanner = ProcfsScanner {
-            proc_dir: test_proc_dir.path().to_path_buf(),
-        };
+        let mut proc_scanner = build_metadata_fetcher(comm_reader, stat_reader);
 
         let process_metadata = proc_scanner
             .fetch_metadata(123)
             .expect("Could not get processes metadata");
 
-        assert_eq!(process_metadata, ProcessMetadata::new(123, "test_cmd".to_string()));
+        assert_eq!(process_metadata.command(), "test_cmd");
     }
 
     #[test]
-    fn test_process_metadata_with_newline() {
-        let test_proc_dir = tempdir().expect("Could not create tmp dir");
+    fn test_process_metadata_has_correct_starttime() {
+        let mut comm_reader = FakeProcessDataReader::<Comm>::new();
+        let mut stat_reader = FakeProcessDataReader::<PidStat>::new();
 
-        create_tempdir("123", test_proc_dir.path()).expect("Could not create process dir");
+        let starttime = 1000;
+        comm_reader.set_pid_sequence(123, vec![Comm::new("test_cmd")]);
+        stat_reader.set_pid_sequence(123, vec![PidStat::new(0, 0, 0, 0, starttime)]);
 
-        let mut comm_file =
-            create_tempfile("comm", test_proc_dir.path().join("123").as_os_str()).expect("Could not create comm file");
+        let mut proc_scanner = build_metadata_fetcher(comm_reader, stat_reader);
 
-        comm_file.write(b"test_cmd\n").expect("Could not write to comm file"); // The process 123's command is test_cmd
-
-        let proc_scanner = ProcfsScanner {
-            proc_dir: test_proc_dir.path().to_path_buf(),
-        };
+        FakeClock::advance_time(1000);
 
         let process_metadata = proc_scanner
             .fetch_metadata(123)
             .expect("Could not get processes metadata");
 
-        assert_eq!(process_metadata, ProcessMetadata::new(123, "test_cmd".to_string()));
+        let clock_ticks = clock_ticks().unwrap();
+        let expected_spawn_time = proc_scanner.boot_time + Duration::from_secs(starttime / clock_ticks);
+
+        assert_eq!(process_metadata.running_span().begin(), expected_spawn_time);
     }
 
     #[test]
     fn test_get_metadata_with_invalid_pid() {
-        let test_proc_dir = tempdir().expect("Could not create tmp dir");
+        let mut comm_reader = FakeProcessDataReader::<Comm>::new();
+        let stat_reader = FakeProcessDataReader::<PidStat>::new();
 
-        let proc_scanner = ProcfsScanner {
-            proc_dir: test_proc_dir.path().to_path_buf(),
-        };
+        comm_reader.make_pid_fail(123);
+
+        let mut proc_scanner = build_metadata_fetcher(comm_reader, stat_reader);
 
         let process_metadata_ret = proc_scanner.fetch_metadata(123);
 
