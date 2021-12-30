c6b70e8e3ad62ed459e4226b9a02162688c309e0
theogilbert <theovgilbert@gmail.com>
2021-12-30 13:26:48 +0100

    test: check "too many open file descriptor" cases

---
+++ tests/processes_tests.rs
@@ -0,0 +1,54 @@
+use std::process::Command;
+use std::thread::sleep;
+use std::time::Duration;
+
+use rstest::{fixture, rstest};
+
+use spv::core::probe::Probe;
+use spv::core::process::ProcessCollector;
+use spv::procfs::cpu_probe::CpuProbe;
+use spv::procfs::process::ProcfsScanner;
+
+#[fixture]
+fn collector() -> ProcessCollector {
+    let process_scanner = ProcfsScanner::new().expect("Could not create procfs scanner");
+    ProcessCollector::new(Box::new(process_scanner))
+}
+
+#[fixture]
+fn probe() -> CpuProbe {
+    CpuProbe::new().expect("Could not create cpu probe")
+}
+
+#[rstest]
+fn test_should_not_fail_due_to_too_many_open_files_over_time(mut collector: ProcessCollector, mut probe: CpuProbe) {
+    (0..10).for_each(|_| {
+        spawn_processes(100, "sleep 0.1");
+        collector.collect_processes().expect("Could not collect processes");
+        probe
+            .probe_processes(&collector.running_pids())
+            .expect("Error running processes");
+        sleep(Duration::from_millis(200));
+    });
+}
+
+#[rstest]
+fn test_should_not_fail_due_to_too_many_open_files_at_once(mut collector: ProcessCollector, mut probe: CpuProbe) {
+    spawn_processes(2000, "sleep 1");
+
+    collector.collect_processes().expect("Could not collect processes");
+
+    probe
+        .probe_processes(&collector.running_pids())
+        .expect("Error running processes");
+}
+
+fn spawn_processes(count: usize, cmd: &str) {
+    (0..count).for_each(|_| {
+        Command::new("sh")
+            .arg("-c")
+            .arg(cmd)
+            .spawn()
+            .expect("Could not launch child process");
+    })
+}
